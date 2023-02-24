79cae4609643ea50ad919d60a7f03e9cba19cccf
AmadorDev <amadorDev@gmail.com>
2023-02-24 10:16:16 -0500

    SS

---
+++ BotComers/Resource/gimnasio/Marketing.js
@@ -0,0 +1,28 @@
+﻿/****************************** menus emails ****************************/
+function evemt_changeMenu(e, num) {
+    switch (num) {
+        case 1:
+            var d = document.getElementById("Configuracion_EnvioCorreos");
+            d.style.display = 'block';
+
+            document.querySelectorAll(".item_menu_sd").forEach((item, index) => {
+                item.classList.remove("active")
+            })
+            e.classList.add("active");
+            document.getElementById("Dconfiguration").style.display = 'none';
+            break;
+        case 2:
+            var d = document.getElementById("Dconfiguration");
+            d.style.display = 'block';
+            document.querySelectorAll(".item_menu_sd").forEach((item, index) => {
+                item.classList.remove("active")
+            })
+            e.classList.add("active");
+            document.getElementById("Configuracion_EnvioCorreos").style.display = 'none'
+            break;
+        default:
+            break;
+    }
+
+
+}
